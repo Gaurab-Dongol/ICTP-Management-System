1aad9e72f2d735dfd448fe056a2442bfdd7751aa
Krupali Valand <krupalivaland95@gmail.com>
2020-06-14 16:32:02 +1000

    database name

--- inc/config.php
+++ inc/config.php
@@ -2,12 +2,9 @@
 	session_start(); // we will use it later  to store logged in user information such as username
 	define("HOST","localhost");
 	define("DB_USER","root");
-	define("DB_PASS","root");
-	define("DB_NAME","ICTP-MNG-SYS");
-	
-	
+	define("DB_PASS","");
+	define("DB_NAME","demo");
 	$conn = mysqli_connect(HOST,DB_USER,DB_PASS,DB_NAME);
-	
 	if(!$conn)
 	{
 		die(mysqli_error());
