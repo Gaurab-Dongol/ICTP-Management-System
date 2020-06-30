fa8327b3cb5fc0a5cfeed7cfc90408a7e62139d3
mike <michaelchristianabay@gmail.com>
2020-06-30 21:27:22 +1000

    db

--- weeklydiary.php
+++ weeklydiary.php
@@ -9,7 +9,31 @@
 	require_once('inc/config.php');
     require_once('layouts/header.php');
     
+    $err_msg = "";
+    $m ="";
+    $UID = $_GET['UID'];
     
+    if(isset($_POST['submit'])){
+
+        $sql = "UPDATE diary SET ManagerRemarks = ?, status = ? where id = ? "; 
+        //$sql = "INSERT INTO test (name)VALUES (?)"; 
+        if($stmt = mysqli_prepare($conn, $sql)) {
+            mysqli_stmt_bind_param($stmt, "sss" , $param_mr ,$param_stat, $param_id);
+            
+            $param_stat = trim($_POST["selectstatus"]);
+            $param_mr = trim($_POST["rmarks"]);
+            //$param_mr = 'ac'; 
+            $param_id = trim($_POST["diaryid"]); 
+            //$param_id = trim($_GET["ID"]); 
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to add page
+                header("location: weeklydiary.php?UID=$UID");
+                echo "records have been updated";
+            } else{
+                echo "not working";
+            }
+        }
+    }    
 
 ?>
 
@@ -29,6 +53,8 @@
 <div class="row">
     <div class="col-lg-12">
         <h2 class="title-1 m-b-25">Weekly Diary</h2>
+       
+        
         <div class="table-responsive table--no-card m-b-40">
             <table class="table table-borderless table-striped table-earning">
             <thead>
@@ -45,9 +71,9 @@
                 </thead>
                 <tbody>
                 <?php
-                   $query="select concat(b.FirstName , ' ', b.LastName) as 'fullname', a.Task_StartDate, a.Task_EndDate, a.TaskDesc, a.status, a.ManagerRemarks from diary a inner join student b on a.studentid = b.studentid inner join internship c on a.internshipid = c.internshipid";
+                   $query="select concat(b.FirstName , ' ', b.LastName) as 'fullname', a.Task_StartDate, a.Task_EndDate, a.TaskDesc, a.status, a.ManagerRemarks, a.id from diary a inner join student b on a.studentid = b.studentid inner join internship c on a.internshipid = c.internshipid";
                    $rs = mysqli_query($conn,$query);
-                   $count = 0;
+                
                        foreach($rs as $row){
                 ?>   
                     <tr>
@@ -55,6 +81,7 @@
                         <td><?php echo $row["Task_StartDate"]?></td>
                         <td><?php echo $row["Task_EndDate"]?></td>
                         <td><?php echo $row["TaskDesc"]?></td>
+                        <form action="" method="POST">  
                         <td>
                             <select name="selectstatus" id="selectstatus" class="form-control" style="width: 120px;">
                                 
@@ -63,21 +90,31 @@
                                 <option <?php if ($row["status"]=='Rejected')echo ' selected="selected"'?>>Rejected</option>
                               </select>
                         </td>
-                        <td>
-                            <input type="text" id="rmarks" placeholder="Enter your Remarks" class="form-control" style="width: 200px;">
-
+                        <td> 
+                        
+                            <input class="form-control" type="text" name="rmarks" placeholder="Enter your Remarks" style="width: 200px;">
+                          
+            
                         </td>
-                        <td>   <div class="form-actions form-group">
-                                                <button type="submit" 
+
+                        <td> 
+                    
+                        <div class="form-actions form-group">
+                        <input type="hidden" name="diaryid" id="diaryid" value="<?php echo $row["id"]; ?>"> 
+                                                <button type="submit" name="submit" 
                                                 class="btn au-btn-icon au-btn--green btn-sm">
-                                                <a href="addcontact.php?UID=<?php echo $_GET['UID']?>"> UPDATE</a></button>             
-                                    </div>  </td>
+                                                  submit</button>             
+                                    </div>  
+                                    
+                                </td>
+                                </form>        
                     </tr>
 
                     <?php }?> 
                 </tbody>
              </table>
-        </div>       
+        </div>   
+                     
     </div>
     </div>
 </div>
