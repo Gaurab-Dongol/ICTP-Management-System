765345dbc12b2363fbc30d481f97fa933a35add9
mike <michaelchristianabay@gmail.com>
2020-07-03 00:06:47 +1000

    db

--- weeklydiary_admin.php
+++ weeklydiary_admin.php
@@ -15,15 +15,17 @@
     
     if(isset($_POST['submit'])){
 
-        $sql = "UPDATE diary SET UCRemarks = ?, status = ? where id = ? "; 
+        $sql = "UPDATE diary SET UCRemarks = ? where studentid = ? and internshipid = ? and status = ?"; 
         //$sql = "INSERT INTO test (name)VALUES (?)"; 
         if($stmt = mysqli_prepare($conn, $sql)) {
-            mysqli_stmt_bind_param($stmt, "sss" , $param_mr ,$param_stat, $param_id);
+            mysqli_stmt_bind_param($stmt, "ssss" , $param_mr,$param_sid,$param_iid, $param_stat);
             
-            $param_stat = trim($_POST["selectstatus"]);
+            $param_stat = trim($_POST["stat"]);
             $param_mr = trim($_POST["ucmarks"]);
             //$param_mr = 'ac'; 
             $param_id = trim($_POST["diaryid"]); 
+            $param_sid = trim($_POST["studentid"]); 
+            $param_iid = trim($_POST["internshipid"]); 
             //$param_id = trim($_GET["ID"]); 
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to add page
@@ -59,57 +61,46 @@
             <table class="table table-borderless table-striped table-earning">
             <thead>
                     <tr>
-                        <th>Student Name</th>
-                        <th>WeekNo</th>
-                        <th>Start Date</th>
-                        <th>Total Hours</th>
+                    <th>Student Name</th>
+                        <th>Aggregated Hours</th>
                         <th>Completed Task</th>
                         <th>Status</th>
-                        <th>Manager Remarks</th>
-                        <!--<th>Unit Coordinator Remarks</th>-->
+                        <th>Unit Coordinator Remarks</th>
                         <th>Update</th>
-                        
                     </tr>
                 </thead>
                 <tbody>
                 <?php
-                   $query="select concat(b.FirstName , ' ', b.LastName) as 'fullname', a.Task_StartDate, a.Task_EndDate, a.TaskDesc, a.totalhours , a.status, a.ManagerRemarks, a.id, a.weekno, a.UCRemarks from diary a inner join student b on a.studentid = b.studentid inner join internship c on a.internshipid = c.internshipid inner join companyuser d on d.companyuserid =c.CompanyUserId where a.status = 'Pending' order by fullname, weekno";
-                   $rs = mysqli_query($conn,$query);
-                
+                  $query="select concat(b.FirstName , ' ', b.LastName) as 'fullname', sum(a.totalhours) as totalhours,  a.status,      GROUP_CONCAT(concat (concat('<h4>WEEK','' ,concat(a.weekno,'','</h4>')), ' ',a.TaskDesc) SEPARATOR '<p></p>') as Task, GROUP_CONCAT(a.Weekno SEPARATOR '//') as Weekno, a.studentid, a.internshipid, a.UCRemarks from diary a inner join student b on a.studentid = b.studentid inner join internship c on a.internshipid = c.internshipid where a.status = 'Pending' group by a.studentid, a.internshipid, a.status order by b.lastname";
+                  $rs = mysqli_query($conn,$query);               
                        foreach($rs as $row){
                 ?>   
                     <tr>
-                        <td><?php echo $row["fullname"]?></td>
-                        <td><?php echo $row["weekno"]?></td>
-                        <td><?php echo $row["Task_StartDate"]?></td>
+                    <td><?php echo $row["fullname"]?></td>
                         <td><?php echo $row["totalhours"]?></td>
-                        <td><?php echo $row["TaskDesc"]?></td>
+
+                        <td><?php echo $row["Task"]?></td>
                         <form action="" method="POST">  
+                        <td > 
+                             <?php echo $row['status']?>   
+                        </td>
+                        <td> 
+                            <input class="form-control" type="text" name="ucmarks" placeholder="<?php echo $row["UCRemarks"]; ?>" style="width: 300px;">   
+                        </td> 
+                         
                         <td>
-                            <select name="selectstatus" id="selectstatus" class="form-control" style="width: 120px;">
-                                
-                                <option <?php if ($row["status"]=='Pending')echo ' selected="selected"'?>>Pending</option>
-                                <option style = 'color:green;' <?php if ($row["status"]=='Approved')echo ' selected="selected"'?>> Approved </option>
-                                <option <?php if ($row["status"]=='Rejected')echo ' selected="selected"'?>>Rejected</option>
-                              </select>
-                        </td>
-                        <td> 
-                            <input class="form-control" type="text" name="rmarks" placeholder="Enter your Remarks" style="width: 300px;">   
-                        </td>
-                        <!--<td> 
-                            <input class="form-control" type="text" name="ucmarks" placeholder="Enter your Comment" style="width: 300px;">   
-                        </td>-->
-                        <td> 
-                    
                         <div class="form-actions form-group">
-                        <input type="hidden" name="diaryid" id="diaryid" value="<?php echo $row["id"]; ?>"> 
-                                                <button type="submit" name="submit" 
-                                                class="btn au-btn-icon au-btn--green btn-sm">
-                                                  submit</button>             
-                                    </div>  
+                        <!--<input type="hidden" name="diaryid" id="diaryid" value="<?php echo $row["id"]; ?>">  -->
+                        <input type="hidden" name="studentid" id="studentid" value="<?php echo $row["studentid"]; ?>">
+                        <input type="hidden" name="internshipid" id="internshipid" value="<?php echo $row["internshipid"]; ?>">
+                        <input type="hidden" name="stat" id="stat" value="<?php echo $row["status"]; ?>">
+                        <button type="submit" name="submit" 
+                        class="btn au-btn-icon au-btn--green btn-sm">
+                        submit</button>             
+                        </div>  
                                     
-                                </td>
-                                </form>        
+                        </td>
+                        </form>             
                     </tr>
 
                     <?php }?> 
@@ -124,16 +115,17 @@
             <thead>
                     <tr>
                         <th>Student Name</th>
-                        <th>Total Hours</th>
+                        <th>Aggregated Hours</th>
                         <th>Completed Task</th>
                         <th>Status</th>
+                        <th>Unit Coordinator Remarks</th>
                         <th>Update</th>
                         
                     </tr>
                 </thead>
                 <tbody>
                 <?php
-                   $query="select concat(b.FirstName , ' ', b.LastName) as 'fullname', sum(a.totalhours) as totalhours,  a.status,      GROUP_CONCAT( concat (concat (concat('<h4>WEEK','' ,concat(a.weekno,'','</h4>')), ' ',a.TaskDesc), 'Manager Remarks: ',a.ManagerRemarks ) SEPARATOR '<p></p>') as Task, GROUP_CONCAT(a.Weekno SEPARATOR '//') as Weekno from diary a inner join student b on a.studentid = b.studentid inner join internship c on a.internshipid = c.internshipid where a.status = 'Approved' group by a.studentid, a.internshipid, a.status";
+                   $query="select concat(b.FirstName , ' ', b.LastName) as 'fullname', sum(a.totalhours) as totalhours,  a.status,      GROUP_CONCAT( concat (concat (concat('<h4>WEEK','' ,concat(a.weekno,'','</h4>')), ' ',a.TaskDesc), 'Manager Remarks: ',a.ManagerRemarks ) SEPARATOR '<p></p>') as Task, GROUP_CONCAT(a.Weekno SEPARATOR '//') as Weekno, a.studentid, a.internshipid, a.UCRemarks from diary a inner join student b on a.studentid = b.studentid inner join internship c on a.internshipid = c.internshipid where a.status != 'Pending' group by a.studentid, a.internshipid, a.status order by b.lastname";
                    $rs = mysqli_query($conn,$query);
                 
                        foreach($rs as $row){
@@ -144,22 +136,26 @@
 
                         <td><?php echo $row["Task"]?></td>
                         <form action="" method="POST">  
-                        <td> 
-                            <input class="form-control" type="text" name="status" placeholder= "<?php echo $row['status']?>"  style="width: 100px;">   
+                        <td > 
+                             <?php echo $row['status']?>   
                         </td>
-                      
-                       
                         <td> 
-                    
+                            <input class="form-control" type="text" name="ucmarks" placeholder="<?php echo $row["UCRemarks"]; ?>" style="width: 300px;">   
+                        </td> 
+                         
+                        <td>
                         <div class="form-actions form-group">
-                        <input type="hidden" name="diaryid" id="diaryid" value="<?php echo $row["id"]; ?>"> 
-                                                <button type="submit" name="submit" 
-                                                class="btn au-btn-icon au-btn--green btn-sm">
-                                                  submit</button>             
-                                    </div>  
+                        <!--<input type="hidden" name="diaryid" id="diaryid" value="<?php echo $row["id"]; ?>">  -->
+                        <input type="hidden" name="studentid" id="studentid" value="<?php echo $row["studentid"]; ?>">
+                        <input type="hidden" name="internshipid" id="internshipid" value="<?php echo $row["internshipid"]; ?>">
+                        <input type="hidden" name="stat" id="stat" value="<?php echo $row["status"]; ?>">
+                        <button type="submit" name="submit" 
+                        class="btn au-btn-icon au-btn--green btn-sm">
+                        submit</button>             
+                        </div>  
                                     
-                                </td>
-                                </form>        
+                        </td>
+                        </form>        
                     </tr>
 
                     <?php }?> 
