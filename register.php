d958c569904a229b4506531b50c3fe114ece0c6a
mike <michaelchristianabay@gmail.com>
2020-07-03 03:33:06 +1000

    db

--- register.php
+++ register.php
@@ -178,11 +178,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                 </div>
                                 <div class="form-group">
                                     <label>Contact Number</label>
-                                    <input class="au-input au-input--full" type="tel" name="contactno" placeholder="+61436XXXXXX">
+                                    <input class="au-input au-input--full" type="tel" name="contactno" placeholder="+61436XXXXXX" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="select" class=" form-control-label">Specialisation</label>
-                                    <select name="specialisation" id="specialisation" class="form-control">
+                                    <select name="specialisation" id="specialisation" class="form-control" required>
                                         <option value="0">Please select</option>
                                         <option value="Networking">Networking</option>
                                         <option value="Distributed Computing">Distributed Computing</option>
@@ -196,7 +196,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                 </div>
                                 <div class="form-group">
                                     <label for="select" class=" form-control-label">Year Enrolled</label>
-                                    <select name="yearenrolled" id="yearenrolled" class="form-control">
+                                    <select name="yearenrolled" id="yearenrolled" class="form-control" required>
                                         <option value="0">Please select</option>
                                         <option value="2018">2018</option>
                                         <option value="2019">2019</option>
